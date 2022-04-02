a5ff138f5904df39b285762612eb377dddde7bc7
elise-b227 <elise.boos@duke.edu>
2022-04-02 15:27:52 -0400

    exploring florida data

---
+++ Code/ExploringData.R
@@ -0,0 +1,11 @@
+require(tidyverse)
+require(dplyr)
+require(lubridate)
+
+florida <- read.csv("./Data/Raw/Discharge_NorthCanal_FL_Raw2.csv", stringsAsFactors = T)
+
+florida_clean <- florida %>%
+  as.Date(datetime, )
+  
+  
+
