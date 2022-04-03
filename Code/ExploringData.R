783a025e3a097a27bdccca97a0360d05cd30b3d2
elise-b227 <elise.boos@duke.edu>
2022-04-03 12:59:40 -0400

    Finally cleaned up the data set ugh why is the date coloumn so annoying

--- Code/ExploringData.R
+++ Code/ExploringData.R
@@ -2,10 +2,16 @@ require(tidyverse)
 require(dplyr)
 require(lubridate)
 
-florida <- read.csv("./Data/Raw/Discharge_NorthCanal_FL_Raw2.csv", stringsAsFactors = T)
+florida <- read.csv("./Data/Raw/Discharge_NorthCanal_FL_Raw3.csv", stringsAsFactors = T)
 
 florida_clean <- florida %>%
-  as.Date(datetime, "%m/%d/%y")
+  mutate(Date = mdy(datetime)) %>%
+  mutate(Discharge = X30943_00060) %>%
+  mutate(State = "Florida") %>%
+  select(State, site_no, Date, Discharge)
+
+
 
   
+
 
