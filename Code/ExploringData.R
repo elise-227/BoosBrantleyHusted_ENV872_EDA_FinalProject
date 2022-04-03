b341dd8141bbe1e60b5fbb7debec778b6ccf2ec2
elise-b227 <elise.boos@duke.edu>
2022-04-03 12:29:33 -0400

    date coloumn issues

--- Code/ExploringData.R
+++ Code/ExploringData.R
@@ -5,7 +5,7 @@ require(lubridate)
 florida <- read.csv("./Data/Raw/Discharge_NorthCanal_FL_Raw2.csv", stringsAsFactors = T)
 
 florida_clean <- florida %>%
-  as.Date(datetime, )
-  
+  as.Date(datetime, "%m/%d/%y")
+
   
 
