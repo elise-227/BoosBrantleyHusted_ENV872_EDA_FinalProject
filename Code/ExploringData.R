6b837edb9db96d91af5001374b3d5ddf5c885a91
elise-b227 <elise.boos@duke.edu>
2022-04-03 13:09:16 -0400

    filter out hurricane months

--- Code/ExploringData.R
+++ Code/ExploringData.R
@@ -10,7 +10,10 @@ florida_clean <- florida %>%
   mutate(State = "Florida") %>%
   select(State, site_no, Date, Discharge)
 
-
+florida_hurricane <- florida_clean %>%
+  mutate(month = month(Date)) %>%
+  filter(month >= 6 & month <= 10) %>%
+  select(1:4)
 
   
 
