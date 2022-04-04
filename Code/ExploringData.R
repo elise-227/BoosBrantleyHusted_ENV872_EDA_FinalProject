6226ad1aa2d89495d0b2e590f2c039c77034f263
elise-b227 <elise.boos@duke.edu>
2022-04-03 21:09:02 -0400

    running initial time series on florida data

--- Code/ExploringData.R
+++ Code/ExploringData.R
@@ -1,6 +1,10 @@
 require(tidyverse)
 require(dplyr)
 require(lubridate)
+library(trend)
+library(zoo)
+library(Kendall)
+library(tseries)
 
 florida <- read.csv("./Data/Raw/Discharge_NorthCanal_FL_Raw3.csv", stringsAsFactors = T)
 
@@ -13,8 +17,29 @@ florida_clean <- florida %>%
 florida_hurricane <- florida_clean %>%
   mutate(month = month(Date)) %>%
   filter(month >= 6 & month <= 10) %>%
-  select(1:4)
+  group_by(Date) %>%
+  summarise(Daily_Discharge = mean(Discharge)) %>%
+  mutate(State = "Florida")
 
-  
 
 
+ggplot(florida_hurricane, aes(x = Date, y = Daily_Discharge))+
+  geom_point()
+
+f_month <- month(first(florida_hurricane$Date))
+f_year <- year(first(florida_hurricane$Date))
+florida_month_ts <- ts(florida_hurricane$Daily_Discharge,
+                   start=c(f_year,f_month),
+                   frequency= 7) 
+
+florida_month_trend <- trend::smk.test(florida_month_ts)
+florida_month_trend
+summary(florida_month_trend)
+
+
+#day??
+florida_hurricane_ts <- ts(florida_hurricane$Daily_Discharge, start=c(f_year,f_month), frequency = 153)  
+florida_hurricane_ts
+
+florida_hurricane_Decomposed <- stl(florida_hurricane_ts, s.window = "periodic")
+plot(florida_hurricane_Decomposed)
