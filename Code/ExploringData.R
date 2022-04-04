515a9cdef286797468c9ec1e6d28911e4e62ffca
elise-b227 <elise.boos@duke.edu>
2022-04-04 15:18:03 -0400

    New florida dataset - idk what im doing anymore :(

--- Code/ExploringData.R
+++ Code/ExploringData.R
@@ -8,41 +8,46 @@ library(tseries)
 
 florida <- read.csv("./Data/Raw/Discharge_NorthCanal_FL_Raw3.csv", stringsAsFactors = T)
 
-florida_clean <- florida %>%
-  mutate(Date = mdy(datetime)) %>%
-  mutate(Discharge = X30943_00060) %>%
+florida_part2 <- read.csv("./Data/Raw/florida_round2.csv", stringsAsFactors = T)
+florida_part2_join <- read.csv("./Data/Raw/florida_round2_1.csv", stringsAsFactors = T)
+
+join <- rbind(florida_part2, florida_part2_join)
+
+florida_clean <- join %>%
+  mutate(Date = ymd(Date)) %>%
   mutate(State = "Florida") %>%
-  select(State, site_no, Date, Discharge)
+  select(State, Date, Discharge)
 
 florida_hurricane <- florida_clean %>%
-  mutate(month = month(Date)) %>%
-  filter(month >= 6 & month <= 10) %>%
   group_by(Date) %>%
   summarise(Daily_Discharge = mean(Discharge)) %>%
+  mutate(Daily_Discharge_clean = zoo::na.approx(Daily_Discharge)) %>%
+  mutate(month = month(Date)) %>%
+  filter(month >= 6 & month <= 10) %>%
   mutate(State = "Florida")
 
 florida_hurricane_edit <- florida_hurricane %>%
   mutate(year = year(Date)) %>%
-  filter(year >= 1991) %>%
-  mutate(Daily_Discharge_clean = zoo::na.approx(Daily_Discharge))
+  filter(year >= 1990) 
 
 ggplot(florida_hurricane_edit, aes(x = Date, y = Daily_Discharge))+
   geom_point()
 
-f_month <- month(first(florida_hurricane_edit$Date))
-f_year <- year(first(florida_hurricane_edit$Date))
+f_month <- month(first(florida_hurricane$Date))
+f_year <- year(first(florida_hurricane$Date))
 
 #florida_month_ts <- ts(florida_hurricane_edit$Daily_Discharge,
                  #  start=c(f_year,f_month),
                 #   frequency= 7) 
 
-florida_month_trend <- trend::smk.test(florida_month_ts)
-florida_month_trend
-summary(florida_month_trend)
+#florida_month_trend <- trend::smk.test(florida_month_ts)
+#florida_month_trend
+#summary(florida_month_trend)
 
+unique(florida_hurricane_edit$year)
 
 #day??
-florida_hurricane_ts <- ts(florida_hurricane_edit$Daily_Discharge_clean, start=c(1991,6), frequency = 153)  
+florida_hurricane_ts <- ts(florida_hurricane$Daily_Discharge_clean, start=c(1987,6), frequency = 153)  
 florida_hurricane_ts
 
 florida_hurricane_Decomposed <- stl(florida_hurricane_ts, s.window = "periodic")
@@ -58,7 +63,7 @@ florida_hurricane_Decomposed_Components <- florida_hurricane_Decomposed_Componen
 #run time series on new data with seasonal removed
 
 nonseasonal_data_florida_ts <- ts(florida_hurricane_Decomposed_Components$data,
-                                start=c(1991,6),
+                                start=c(1987,6),
                                 frequency=153) 
 plot(nonseasonal_data_florida_ts)
 
