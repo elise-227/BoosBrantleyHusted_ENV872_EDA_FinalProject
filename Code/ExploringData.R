b935b5a2a3a95bd0bda5cb56b78ac576acc821c9
elise-b227 <elise.boos@duke.edu>
2022-04-04 22:47:09 -0400

    This code is an absolute mess and no one should read it till its cleaned up.

--- Code/ExploringData.R
+++ Code/ExploringData.R
@@ -11,6 +11,22 @@ florida <- read.csv("./Data/Raw/Discharge_NorthCanal_FL_Raw3.csv", stringsAsFact
 florida_part2 <- read.csv("./Data/Raw/florida_round2.csv", stringsAsFactors = T)
 florida_part2_join <- read.csv("./Data/Raw/florida_round2_1.csv", stringsAsFactors = T)
 
+
+Date  <- seq(as.Date("1987-01-01"),as.Date("2021-10-31"), by = "days")
+Dates <- as.data.frame(Date)
+
+date_join <- Dates %>%
+  mutate(month = month(Date)) %>%
+  filter(month >= 6 & month <= 10) %>%
+  select(1)
+
+fh_clean <-left_join(date_join, florida_hurricane, by = "Date")
+summary(fh_clean$Daily_Discharge)
+
+fh_clean <- fh_clean %>%
+  select(1,2) %>%
+  mutate(Daily_Discharge_clean = zoo::na.approx(Daily_Discharge))
+
 join <- rbind(florida_part2, florida_part2_join)
 
 florida_clean <- join %>%
@@ -26,7 +42,7 @@ florida_hurricane <- florida_clean %>%
   filter(month >= 6 & month <= 10) %>%
   mutate(State = "Florida")
 
-florida_hurricane_edit <- florida_hurricane %>%
+florida_1990_edit <- fh_clean %>%
   mutate(year = year(Date)) %>%
   filter(year >= 1990) 
 
@@ -44,10 +60,10 @@ f_year <- year(first(florida_hurricane$Date))
 #florida_month_trend
 #summary(florida_month_trend)
 
-unique(florida_hurricane_edit$year)
+
 
 #day??
-florida_hurricane_ts <- ts(florida_hurricane$Daily_Discharge_clean, start=c(1987,6), frequency = 153)  
+florida_hurricane_ts <- ts(florida_1990_edit$Daily_Discharge_clean, start=c(1990,6), frequency = 153)  
 florida_hurricane_ts
 
 florida_hurricane_Decomposed <- stl(florida_hurricane_ts, s.window = "periodic")
@@ -63,7 +79,7 @@ florida_hurricane_Decomposed_Components <- florida_hurricane_Decomposed_Componen
 #run time series on new data with seasonal removed
 
 nonseasonal_data_florida_ts <- ts(florida_hurricane_Decomposed_Components$data,
-                                start=c(1987,6),
+                                start=c(1990,6),
                                 frequency=153) 
 plot(nonseasonal_data_florida_ts)
 
