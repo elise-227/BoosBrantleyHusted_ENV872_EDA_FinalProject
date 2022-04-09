e3b110a53523c8e29bbbe76b21959e24bd844aa6
Kelsey Husted: Original <kelseyhustedOriginal@Kelseys-MacBook-Pro-4.local>
2022-04-09 11:18:29 -0400

    Created NY processed csv, created nonseasonal plot for presentation

--- Code/NY_Data.R
+++ Code/NY_Data.R
@@ -18,25 +18,26 @@ NY_AnnualPeakDischarge <- read.csv("Data/Raw/NYAnnualPeakFlowDischarge.csv")
 NY_Discharge$Date <- as.Date(NY_Discharge$Date, format = "%m/%d/%Y")
 NY_AnnualPeakDischarge$Date <- as.Date(NY_AnnualPeakDischarge$Date, format = "%m/%d/%Y")
 
-#Clean Up Data
-#rename and remove columns
+#CWrangle Data
+##rename and remove columns
 NY_Discharge.processed <-
   NY_Discharge %>%
   rename(Agency = agency_,
          SiteNumber = cd.......site_no,
          MeanDischarge = X104599_00060_00003) %>%
   select(Agency, SiteNumber, MeanDischarge, Month, Date, Year) %>%
-  drop_na() %>%
   filter(Month > 5, Month < 11,
-         #Year >= 90)
          Date >= "1990-06-01")
 view(NY_Discharge.processed)
 
-#log mean discharge
-NY_Discharge.processed$LogDischarge = log(NY_Discharge.processed$MeanDischarge)
+#log mean discharge (not necessary at the moment; will use later on in a plot)
+#NY_Discharge.processed$LogDischarge = log(NY_Discharge.processed$MeanDischarge)
+
+#saving processed file as a csv
+write.csv(NY_Discharge.processed, "./Data/Processed/NY_DischargeData_Processed", row.names = FALSE)
 
 
-#Daily Discharge Plot logged
+#Initial Visualization of Data: Daily Discharge Plot (logged)
 ggplot(NY_Discharge.processed, aes(x = Date, y = LogDischarge)) +
   geom_point(color = "blue", size = 0.7) +
   geom_smooth(method = lm, color = "black") +
@@ -46,15 +47,27 @@ ggplot(NY_Discharge.processed, aes(x = Date, y = LogDischarge)) +
   theme(axis.text.x = element_text(angle = 90)) 
   
 
+# filling missing data with linear interpolation
+NY_Daily.Discharge.Clean <-
+  NY_Discharge.processed %>%
+  mutate(MeanDischargeClean = zoo::na.approx(MeanDischarge))
+view(NY_Daily.Discharge.Clean)
+
+#log mean discharge
+NY_Daily.Discharge.Clean$LogMeanDischarge = log(NY_Daily.Discharge.Clean$MeanDischargeClean)
+view(NY_Daily.Discharge.Clean)
+
+
 
 #NY Daily Discharge time-series
-NY_DailyDischarge.ts <- ts(NY_Discharge.processed$LogDischarge, start = c(1990,01), frequency = 153)
+NY_DailyDischarge.ts <- ts(NY_Daily.Discharge.Clean$MeanDischargeClean, start = c(1990,01), frequency = 153)
 head(NY_DailyDischarge.ts, 10)
 #Decompose
 NY_DailyDischarge.decompose <- stl(NY_DailyDischarge.ts, s.window = "periodic")
 plot(NY_DailyDischarge.decompose)
 
-
+--------------------------------------------------------------------------------------
+#Messing with Interactive time-series functions
 #create time-series object
 NY_DischargeTimeSeries <- xts(x = NY_Discharge.processed$MeanDischarge,
                             order.by = NY_Discharge.processed$Date)
@@ -67,16 +80,18 @@ NY_Interact_time
 NY_Interact_time2 <- dygraph(discharge_timeSeries) %>%
   dyRangeSelector()
 NY_Interact_time2
-
+--------------------------------------------------------------------------------------
+  
 
 #Nonseasonal MannKendall
 NY_Components <- as.data.frame(NY_DailyDischarge.decompose$time.series[,2:3])
 view(NY_Components)
 
+#Add in a date column
 NY_Components <- mutate(NY_Components,
-                        Observed = NY_Discharge.processed$MeanDischarge,
                         Date = NY_Discharge.processed$Date)
 
+#Adding non-seasonal component to dataframe
 NY_Combine_Nonseasonal <- mutate(NY_Components, Nonseasonal = NY_Components$trend + NY_Components$remainder)
 view(NY_Combine_Nonseasonal)
 
@@ -92,14 +107,27 @@ NY_Trend.Nonseasonal
 #reject null hypothesis and we state that there is a trend
 
 
-NY_NonSeasonal.Plot <- autoplot(NY_Nonseasonal.ts) +
+#trying to create a plot for the ts but not working! (I'll try ggplot instead below as well)
+#autoplot version
+NY_NonSeasonal.Plot1 <- autoplot(NY_Nonseasonal.ts) +
   scale_y_log10() +
   labs(x = "Date", y = "Non-Seasonal Daily Discharge (cfs)", 
        title = "Non-Seasonal Mean Daily Discharge During Hurricane Seasons (1990-2021)", 
-       subtitle = "Cold Spring Harbor, New York") 
-  #geom_smooth(method = "lm")
-print(NY_NonSeasonal.Plot)
+       subtitle = "Cold Spring Harbor, New York") +
+  geom_smooth(method = "lm")
+print(NY_NonSeasonal.Plot1)
  
-# running Mann Kendall on time series with seasonality removed
-MannKendall(NonSeasonal.DailyDischarge.ts)
+#ggplot version (working)
+NY_NonSeasonal.Plot2 <- ggplot(NY_Combine_Nonseasonal, aes(x = Date, y = Nonseasonal)) +
+  scale_y_log10() +
+  geom_point(color = "blue", size = 0.6) +
+  labs(x = "Date", y = "Non-Seasonal Daily Discharge (cfs)", 
+       title = "Non-Seasonal Mean Daily Discharge During Hurricane Seasons (1990-2021)", 
+       subtitle = "Cold Spring Harbor, New York") +
+  geom_smooth(method = "lm", color = "black") +
+  scale_x_date(date_labels = "%Y", date_breaks = "2 year") +
+  theme(axis.text.x = element_text(angle = 90)) 
+                               
+print(NY_NonSeasonal.Plot2)
+
 
