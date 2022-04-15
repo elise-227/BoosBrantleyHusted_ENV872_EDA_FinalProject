fbcf2b4ad581ab2c1d176c55dd1e69cdcda9fabc
elise-b227 <elise.boos@duke.edu>
2022-04-15 12:22:09 -0400

    exploring florida code

---
+++ Code/Florida_Data_exploration.R
@@ -0,0 +1,13 @@
+require(lubridate)
+#load data
+florida_1990 <- read.csv("./Data/Processed/florida_hurricane_1990_processed.csv", stringsAsFactors = T)
+#Date column
+florida_1990 <- florida_1990 %>%
+  mutate(Date = ymd(Date))
+
+class(florida_1990$Date)
+
+#plot discharge before running time series on the log scale
+ggplot(florida_1990, aes(x = Date, y = log(Daily_Discharge_clean))+
+  geom_point(color = "deeppink1")+
+  geom_smooth(method = lm, color = "black")
