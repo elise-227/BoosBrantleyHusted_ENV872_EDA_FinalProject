098a5212d5c91c039d6daa9cbddad40b7cfcb50b
elise-b227 <elise.boos@duke.edu>
2022-04-15 18:39:24 -0400

    cleaning

--- Code/Florida_Data_exploration.R
+++ Code/Florida_Data_exploration.R
@@ -6,9 +6,6 @@ florida_1990 <- read.csv("./Data/Processed/florida_hurricane_1990_processed.csv"
 florida_1990 <- florida_1990 %>%
   mutate(Date = ymd(Date))
 
-class(florida_1990$Date)
-
-#plot discharge before running time series on the log scale
 ggplot(florida_1990, aes(x = Date, y = log(Daily_Discharge_clean)))+
   geom_point(color = "deeppink1")+
   geom_smooth(method = lm, color = "black")+
